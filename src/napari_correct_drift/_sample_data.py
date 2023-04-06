f43514181705c220f20e9d1be7bf259377a56218
Christoph Sommer <christoph.sommer@ist.ac.at>
2023-04-06 17:40:24 +0200

    added example data and using them for testing in absolute shift estimation

--- src/napari_correct_drift/_sample_data.py
+++ src/napari_correct_drift/_sample_data.py
@@ -10,7 +10,114 @@ from __future__ import annotations
 
 import numpy as np
 from skimage.draw import disk
-import tifffile
+from skimage.morphology import ball, binary_dilation
+
+
+class Example_TZYX:
+    def __init__(self):
+        deltas_3d = (
+            [[1, 6, 4]] * 4
+            + [[-1, 4, 1]] * 6
+            + [[1, 2, -3]] * 6
+            + [[0, -2, 7]] * 4
+            + [[0, 2, -5]] * 4
+            + [[0, 7, 0]] * 4
+            + [[0, 0, -7]] * 3
+        )
+
+        shifts_3d = np.cumsum(deltas_3d, 0)
+        shifts_3d = np.vstack(
+            [
+                [
+                    0,
+                    0,
+                    0,
+                ],
+                shifts_3d,
+            ]
+        )
+
+        self.shape = (32, 16, 128, 80)
+        self.start_pos = (8, 24, 32)
+        self.deltas = deltas_3d
+        self.shifts = shifts_3d
+
+    def create(self):
+        deltas = np.asarray(self.deltas)
+
+        T, Z, Y, X = self.shape
+        img = np.zeros((T, Z, Y, X), dtype="bool")
+
+        t = 0
+        img[t][self.start_pos] = 1
+        img[t] = binary_dilation(img[t], ball(3))
+
+        pos = np.array(self.start_pos, dtype="int32")
+
+        for d in deltas:
+            t += 1
+            pos += d
+            img[t][tuple(pos)] = 1
+            img[t] = binary_dilation(img[t], ball(3))
+
+        img = img.astype("uint8") * (255 - 32)
+
+        for t in range(T):
+            img[t] += (np.random.rand(Y, X) * 32).astype("uint8")
+
+        return img
+
+
+class Example_TYX:
+    def __init__(self):
+        deltas_2d = [[8, 8]] * 10
+        deltas_2d += [[-2, 6]] * 5
+        deltas_2d += [[6, 3]] * 10
+        deltas_2d += [[0, 7]] * 3
+        deltas_2d += [[11, -3]] * 3
+
+        deltas_2d = [(0, y, x) for y, x in deltas_2d]
+
+        shifts_2d = np.cumsum(deltas_2d, 0)
+        shifts_2d = np.vstack([[0, 0, 0], shifts_2d])
+
+        self.shape = (32, 256, 256)
+        self.start_pos = (32, 32)
+        self.deltas = deltas_2d
+        self.shifts = shifts_2d
+
+    def create(self):
+        deltas = np.asarray(self.deltas)
+
+        T, Y, X = self.shape
+        img = np.zeros((T, Y, X), dtype="uint8")
+
+        for t in range(T):
+            img[t] += (np.random.rand(Y, X) * 64).astype("uint8")
+
+        t = 0
+        rr, cc = disk(self.start_pos, 8, shape=(256, 256))
+        img[t, rr, cc] = 255
+
+        pos = np.array(self.start_pos)
+
+        for d in deltas:
+            t += 1
+            pos += d[1:]
+            rr, cc = disk(pos, 8, shape=(256, 256))
+            img[t, rr, cc] = 255
+
+        return img
+
+
+class Example_TCYX(Example_TYX):
+    def create(self):
+        img = super().create()
+
+        img = np.stack([img, img[::-1]], axis=1)
+        self.shape = img.shape
+
+        return img
 
 
 def make_sample_data():
@@ -21,33 +128,17 @@ def make_sample_data():
     # add_image_kwargs
     # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
 
-    T = 32
-    X = 256
-    Y = 256
-
-    img = np.zeros((T, Y, X), dtype="uint8")
-    for t in range(T):
-        img[t] += (np.random.rand(Y, X) * 64).astype("uint8")
-
-    pos = np.array([32, 32], dtype="float64")
-
-    t_ = 0
-    for t in range(12):
-        pos += 2 * t
-        rr, cc = disk(pos, 8, shape=(256, 256))
-        img[t_, rr, cc] = 255
-        t_ += 1
-
-    for t in range(10):
-        pos += np.array([t * 1, -t * 2])
-        rr, cc = disk(pos, 8, shape=(256, 256))
-        img[t_, rr, cc] = 255
-        t_ += 1
-
-    for t in range(10):
-        pos += np.array([-t * 2, -t * 1 / 2])
-        rr, cc = disk(pos, 8, shape=(256, 256))
-        img[t_, rr, cc] = 255
-        t_ += 1
-
-    return [(img, {})]
+    return [
+        (
+            Example_TYX().create(),
+            {"name": "test_TYX"},
+        ),
+        (
+            Example_TZYX().create(),
+            {"name": "test_TZYX"},
+        ),
+        (
+            Example_TCYX().create(),
+            {"name": "test_TCYX"},
+        ),
+    ]
